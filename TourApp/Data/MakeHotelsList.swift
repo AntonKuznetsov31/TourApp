d55b6ff74d9d39881b7b7603d9eb2cc655c90e47
Borys Krisko <bvkr@me.com>
2019-08-17 20:13:07 +0300

    working with array

---
+++ TourApp/Data/MakeHotelsList.swift
@@ -0,0 +1,31 @@
+//
+//  MakeHotelsList.swift
+//  TourApp
+//
+//  Created by Борис Крисько on 8/17/19.
+//  Copyright © 2019 AntonKuznetsov. All rights reserved.
+//
+
+import UIKit
+
+class MakeHotelsList {
+   
+    static let data = MakeHotelsList()
+
+
+
+
+func loadData() -> [Tour] {
+    let tours = [
+        Tour(name: "Anita ApartHotel 4*",
+         country: "Greece",
+         city: "Rodos",
+         price: 84014,
+         food: .ro
+        )
+    ]
+    return tours
+}
+
+}
+
--- TourApp/ViewConrollers/MainScreenVC.swift
+++ TourApp/ViewConrollers/MainScreenVC.swift
@@ -18,28 +18,18 @@ class MainTableViewCell: UITableViewCell {
 }
 
 class MainScreenVC: UITableViewController {
+    
+    var tours: [Tour] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tours = MakeHotelsList.data.loadData()
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
+        return tours.count
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -58,50 +48,9 @@ class MainScreenVC: UITableViewController {
         cell.countryLabel.text = testHotel.country
         cell.priceLabel.text = String(testHotel.price)
         cell.foodLabel.text = "AI"
-        
-        
-//        cell.textLabel?.text = testHotel.name
-//        cell.detailTextLabel?.text = testHotel.country
-//        cell.imageView?.image = UIImage(named: "paris1")
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
