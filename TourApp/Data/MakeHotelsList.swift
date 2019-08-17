c5211576338f359782c7ba3a7f38952350958614
Borys Krisko <bvkr@me.com>
2019-08-17 21:06:50 +0300

    everything is done

--- TourApp/Data/MakeHotelsList.swift
+++ TourApp/Data/MakeHotelsList.swift
@@ -20,7 +20,52 @@ func loadData() -> [Tour] {
          price: 84014,
          food: "RO",
          image: "AnitaApartHotel"
-        )
+        ),
+        Tour(name: "Irida Hotel 3*",
+             country: "Greece",
+             city: "Heraklion",
+             price: 87309,
+             food: "RO",
+             image: "IridaHotelApartments"
+        ),
+        Tour(name: "Citymax Dubai 3*",
+             country: "UAE",
+             city: "Dubai",
+             price: 78677,
+             food: "BB",
+             image: "CitymaxHotelBurDubai"
+        ),
+        Tour(name: "Acacia Majid 4*",
+             country: "UAE",
+             city: "Ras El Hekma",
+             price: 78402,
+             food: "BB",
+             image: "AcaciabyBinMajid"
+        ),
+        
+        Tour(name: "Copthorne Hotel 4*",
+             country: "UAE",
+             city: "Sharjah",
+             price: 79872,
+             food: "BB",
+             image: "CopthorneHotelSharjah"
+        ),
+        
+        Tour(name: "Hedef Resort & Spa 5*",
+             country: "Turkey",
+             city: "Konakli",
+             price: 81274,
+             food: "UIA",
+             image: "HedefResort&Spa"
+        ),
+        Tour(name: "Campus Hill Hotel 5*",
+             country: "Turkey",
+             city: "Alanya",
+             price: 81274,
+             food: "AI",
+             image: "CampusHillHotel"
+        ),
+        
     ]
     return tours
 }
